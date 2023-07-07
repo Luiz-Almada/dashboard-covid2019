fb06a92c50314cbca269d1a96cbf8852fea2ecad
Tiago dos Santos Carvalho <tiago.carvalho@trf1.jus.br>
2023-07-06 21:51:10 -0300

    Alterado o dashboard de country

--- assets/js/country.js
+++ assets/js/country.js
@@ -1,148 +1,216 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  //Função para preencher os selects do index, recebe id do select e um set com os valores
-  preencheSelectComDadosSet = (id, set)=>{
-    let arrayOrdenado = Array.from(set).sort();
-    let select = document.getElementById(id);
-    let valorPadrao = "Brazil"; // Defina o valor padrão que você deseja
-    //Preenche o select (combo)
-    for(let item of arrayOrdenado){
-      if(item != null)
-        select.innerHTML += `<option value="${item}">${item}</option>`;
-    }
+let json;
+let jsonDiseaseHistoricalAll;
+let canvasLinhas = null; 
 
-    //Seta o valor default (Brazil)
-     for (let i = 0; i < select.options.length; i++) {
-       if (select.options[i].value === valorPadrao) {
-         select.selectedIndex = i;
-         break;
-       }   
-     }  
-  }
-
-  function carregaDados(result) {
-    fullData = result;
-    filteredData = filtrar(fullData)
-    getTotaisKPIs(filteredData);
-  }
-
-  function getTotaisKPIs(result) {
-    let kpiconfirmed = document.getElementById('kpiconfirmed');
-    let kpideaths = document.getElementById('kpideaths');
-    let kpirecovered = document.getElementById('kpirecovered');
-
-    kpiconfirmed.textContent = getTotalItens("confirmed", result).toLocaleString('pt-BR');
-    kpideaths.textContent = getTotalItens("deaths", result).toLocaleString('pt-BR');
-    kpirecovered.textContent = getTotalItens("recovered", result).toLocaleString('pt-BR');
-
-  }
-
-  function getTotalItens(item, data) {
-    const pais = document.getElementById('cmbCountry');
-    return data.reduce(function (total, current) {
-      if (current.Status.toUpperCase() === item.toUpperCase() 
-          && current.Country.toUpperCase() === pais.value.toUpperCase()) 
-      {
-        return total = current.Cases;
-      }
-      return total;
-    }, 0);    
-  }
-
-  function filtrar(data){
-    let pais = document.getElementById("cmbCountry");
-    
-    if(pais.options[pais.selectedIndex].value != ""){
-      result = (data.Countries.filter(item => {
-        if(item.Country != null){
-          return item.Country.toUpperCase().trim() == pais.value.toUpperCase().trim();
-        }
-      return false;
-      }));
-    }
-
-    
-    // if(nome.value != ""){
-    //   resultado = resultado.filter(item => {
-    //     if(item.name != null){
-    //       return item.name.toUpperCase().trim() == nome.value.toUpperCase().trim()
-    //     }
-    //     return false;
-    //   });
-    // }
-    
-    
-    if(result.length == 0){
-      return null;
-    } else {
-      return result;
-    }  
-  }
-
-  requisicao = async () => { 
+//async function carregarJsonAsync() {  // outra Sintaxy
+carregarJsonAsync = async () => {
     //let resAll = await fetch("http://localhost:3001/all", {method: 'GET', redirect: 'follow'})
-    let resAll = await (await fetch("data/all.json"));
-    let json = await resAll.json();
 
-    const elPais = document.getElementById("cmbCountry");
+    jsonDiseaseHistoricalAll = await (await fetch('data/diseaseHistoricalAll.json', { mode: 'no-cors' })).json();
+    carregarCombos();
 
-    if(elPais.length == 0) {
-      const pais = Array.from(new Set(json.Countries.map(item => item.Country)));
-      preencheSelectComDadosSet("cmbCountry", pais);
-    }
+    country = document.getElementById("cmbCountry").options[document.getElementById("cmbCountry").selectedIndex].value
 
-    carregaDados(json);
+    json = await (await fetch(`https://disease.sh/v3/covid-19/historical/${country}/?lastdays=all`)).json();
+
+
+    aplicarFiltroDeDatas();
+    criarGraficoLinhas();
+    carregarDados(json);
+};
+
+criarEventosDom();
+
+carregarJsonAsync();
+
+
+
+function carregarCombos(){
+  const cmbCountry = document.getElementById("cmbCountry");
+  if(cmbCountry.length == 0) {
+    const setPais = Array.from(    new Set(jsonDiseaseHistoricalAll.map((item) => item.country))   );
+    preencherSelectComDadosSet("cmbCountry", setPais);
   }
 
-  document.getElementById("cmbCountry").addEventListener("change", (e)=>{
-    requisicao();
-  })
+}
 
-  document.getElementById("filtro").addEventListener("click", (e)=>{
-    requisicao();;
-  })
 
+function criarEventosDom(){
+  document.getElementById("cmbCountry").addEventListener("change", (e) => {
+    carregarJsonAsync();
+    carregarDados();
+  });
+
+  document.getElementById("cmbData").addEventListener("change", (e) => {
+    carregarJsonAsync();
+    carregarDados();
+  });
+
+  document.getElementById("filtro").addEventListener("click", (e) => {
+    carregarJsonAsync();
+    carregarDados();
+  });
+
+  document.getElementById("date_start").addEventListener("click", (e) => {
+    carregarJsonAsync();
+    carregarDados();
+  });
+
+  document.getElementById("date_end").addEventListener("click", (e) => {
+    carregarJsonAsync();
+    carregarDados();
+  });
+}
+
+
+
+
+
+
+
+function carregarDados() {
+  getTotaisKPIs(json);
+}
+
+
+function getTotaisKPIs(result) {
+  let kpiconfirmed = document.getElementById("kpiconfirmed");
+  let kpideaths = document.getElementById("kpideaths");
+  let kpirecovered = document.getElementById("kpirecovered");
+
+  kpiconfirmed.textContent = getTotalItens(result.timeline.cases).toLocaleString("pt-BR");
+  kpideaths.textContent = getTotalItens(result.timeline.deaths).toLocaleString("pt-BR");
+  kpirecovered.textContent = getTotalItens(result.timeline.recovered).toLocaleString("pt-BR");
+}
+
+function getTotalItens(data) {
+  const cmbCountry = document.getElementById("cmbCountry");
+
+  // Converter o objeto em um array de pares chave-valor
+  const arrayDados = Object.entries(data);
+
+  // Ordenar o array com base nas chaves (datas)
+  //arrayDados.sort((a, b) => new Date(a[0]) - new Date(b[0]));
+  arrayDados.sort((a, b) => new Date(b[0]) - new Date(a[0]));
+
+  // Reconstruir o objeto com base no array ordenado
+  //const dadosOrdenados = Object.fromEntries(arrayDados);
+
+  const valorMaiorData = arrayDados[0][1];
+  return valorMaiorData;
+}
+
+preencherSelectComDadosSet = (idCombo, setDataCombo) => {
+  let arrayOrdenado = Array.from(setDataCombo).sort();
+
+  let select = document.getElementById(idCombo);
+  //Preenche o select (combo)
+  for (let item of arrayOrdenado) {
+    if (item != null)
+      select.innerHTML += `<option value="${item}">${item}</option>`;
+  }
+
+  //Seta o valor default (Brazil)
+  let valorPadrao = "Brazil"; // Defina o valor padrão que você deseja
+  for (let i = 0; i < select.options.length; i++) {
+    if (select.options[i].value === valorPadrao) {
+      select.selectedIndex = i;
+      break;
+    }
+  }
+};
+
+function criarGraficoLinhas(){
+
+  //Tipo de dados
+  cmbData = document.getElementById("cmbData").options[document.getElementById("cmbData").selectedIndex].value.toLowerCase();
+  let arrLabels = Object.keys(eval(`json.timeline.${cmbData}`))
+  let arrValues = Object.values(eval(`json.timeline.${cmbData}`))
   
 
-  let linhas = new Chart(document.getElementById("linhas"), {
-    type: 'line',
+  //criar dados da linha média
+  const soma = arrValues.reduce((acc, valor) => acc + valor, 0);
+  const media = soma / arrValues.length;
+  const arrMedia = Array.from(arrValues, () => media);
+
+  
+  // Verificar se o gráfico já existe e destruí-lo, se necessário
+  if (canvasLinhas) {
+    canvasLinhas.destroy();
+  }
+  canvasLinhas = new Chart(document.getElementById("linhas"), {
+    type: "line",
     data: {
-      labels: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+      labels: arrLabels,
       datasets: [
         {
-          data: [1123, 1109, 1008, 1208, 1423, 1114, 1036],
+          data: arrValues,
           label: "Casos confirmados",
           borderColor: "rgb(60,286,159)",
-          backgroundColor: "rgb(60,286,159,0.1)"
+          backgroundColor: "rgb(60,286,159,0.1)",
         },
         {
-          data: [143, 109, 208, 210, 113, 114, 203],
-          label: "Número de Óbitos",
+          data: arrMedia,
+          label: "Média",
           borderColor: "rgb(255,140,13)",
-          backgroundColor: "rgb(255,140,13,0.1)"
-        }
-      ]    
+          backgroundColor: "rgb(255,140,13,0.1)",
+        },
+      ],
     },
-    options: {  
+    options: {
       responsive: true,
       plugins: {
         legend: {
           display: true,
-          position: 'left', //top, bottom, left, rigth 
+          position: "left", //top, bottom, left, rigth
         },
         title: {
           display: true,
-          text: 'Curva de COVID'
+          text: "Curva de COVID",
         },
         layout: {
           padding: {
             left: 100,
             rigth: 100,
             top: 50,
-            bottom: 10
-          }
-        }
-      }
-    }
+            bottom: 10,
+          },
+        },
+      },
+    },
   });
+}
 
-})
+
+function aplicarFiltroDeDatas(){
+  const [mes_start, dia_start, ano_start] = document.getElementById("date_start").value.split('-');
+  const dataInicio  = new Date(`${mes_start}/${dia_start}/${ano_start}`);
+  const [mes_end, dia_end, ano_end] = document.getElementById("date_end").value.split('-');
+  const dataFim  = new Date(`${mes_end}/${dia_end}/${ano_end}`);
+
+
+  cmbData = document.getElementById("cmbData").options[document.getElementById("cmbData").selectedIndex].value.toLowerCase();
+
+  //const casos = json.timeline.cases;
+  const casos = eval(`json.timeline.${cmbData}`);
+  console.log(casos);
+  const casosFiltrados = {};
+
+  for (const data in casos) {
+    const [mes, dia, ano] = data.split('/'); // Divide a string da data em três partes: mês, dia e ano
+
+    const dataFormatada = new Date(`${mes}/${dia}/${ano}`); // Cria o objeto Date com o formato correto
+
+    const dataInicioFormatada = new Date(dataInicio);
+    const dataFimFormatada = new Date(dataFim);
+
+    if (dataFormatada >= dataInicioFormatada && dataFormatada <= dataFimFormatada) {
+      casosFiltrados[data] = casos[data];
+    }
+}
+
+console.log(casosFiltrados);
+json.timeline.cases = casosFiltrados;
+
+console.log(json);
+}
